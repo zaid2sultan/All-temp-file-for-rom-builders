e64966e7683fe87a7a8f00e13fa105eac08b093b
zaid2sultan <100853058+zaid2sultan@users.noreply.github.com>
2023-09-24 02:20:05 +0530

    Lavender aospa zaid2sultan

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
-# sync rom 
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Amritorock/local_manifest.git --depth 1 -b topaz .repo/local_manifests
+git clone https://github.com/zaid2sultan/local_manifests.git --depth 1 -b aospa13-lav .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
-lunch aospa_r5x-userdebug
+lunch aospa_lavender-userdebug
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
-./rom-build.sh r5x -t userdebug 
+./rom-build.sh lavender -t userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
